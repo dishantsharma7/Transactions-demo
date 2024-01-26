777118b3ff75c877603b73c2731327f0aec13cb2
Dishant Sharma <98343608+dishantsharma7@users.noreply.github.com>
2024-01-27 01:34:05 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,7 @@
 // backend/db.js
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb+srv://admin:hello6969@cluster0.ozijqxv.mongodb.net/paytm")
+mongoose.connect("your-mongo-URI")
 
 // Create a Schema for Users
 const userSchema = new mongoose.Schema({
